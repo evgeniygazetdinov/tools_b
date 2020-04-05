f061f45bd582f0894961b3a8d550fac462a81ae1
evgesha <zplacesound@gmail.com>
2020-04-05 07:47:47 +0300

    add buttons

--- main.py
+++ main.py
@@ -5,21 +5,33 @@ from protect import do_some_protection
 from const import token
 import time
 from telebot import types
+import logging
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-#do_some_protection()
+do_some_protection()
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+   
     bot.reply_to(message, "Напиши мне номер модели, и ты получишь всю необходимую информацию\n" +
                  "Если же ты хочешь добавить свою технику в нашу базу для быстрого поиска клиентов, то сделать это можно по ссылке: " +
-                 "www.sklad-kamaz.ru")
-
-
+                 "www.bot-kamaz.ru")
+    
+    #bot.send_message(message['chat']['id'], "Choose one letter:", reply_markup=markup)
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('a')
+    itembtn2 = types.KeyboardButton('v')
+    itembtn3 = types.KeyboardButton('d')
+    markup.add(itembtn1, itembtn2, itembtn3)
     print(message)
+    bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+
+
     
 
 
