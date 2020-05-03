47ed48db4dfd02f52bb5b5d1699d45720e3ac2cb
evgesha <zplacesound@gmail.com>
2020-05-03 16:22:26 +0300

    lame async try

--- lib/sessions.py
+++ lib/sessions.py
@@ -3,8 +3,10 @@ import os
 import json
 import shutil
 from datetime import datetime
-import threading
-import time 
+import time
+
+
+
 
 class Session(object):
 
@@ -17,31 +19,39 @@ class Session(object):
         if os.path.exists(os.getcwd()+'/session/'+self.username):
             self.user_folder = os.getcwd()+'/session/'+self.username
             self.user_info = self.get_session_details()
-            self.check_user_action()
+            self.save_user_info()
         else:
             self.user_info = {'username':username,'password':password,
             'state': {'login': False, 'created': False,'upload': False,'change_password':False},
             'changer':{'old_password':False,'new_password':False},
             'last_action':datetime.now().strftime('%Y-%m-%d %H:%M')}
-
             self.user_folder = self.create_user_folder()
             self.save_user_info()
-    
-    def check_user_action(self,):
-        threading.Timer(60.0, self.check_user_action).start() # called every minute
+
+
+    def check_user_action(self,job):
         print(self.user_info['last_action'])
 
 
+    def update_last_action(self):
+        self.user_info['last_action'] = datetime.now().strftime('%Y-%m-%d %H:%M')
+
+
     def update_state_user(self,state,value,password=False):
+        self.update_last_action()
         self.user_info['state'][state] = value
         if password:
             self.user_info['password'] = password
         self.save_user_info()
         print(self.user_info)
 
+    def get_user_info_value(self,value):
+        self.update_last_action()
+        return self.user_info[value]
+
+
     def save_user_info(self):
-        self.user_info['last_action'] = datetime.now().strftime('%Y-%m-%d %H:%M')
-        self.check_user_action()
+        self.update_last_action()
         with open(self.user_folder +'/{}.json'.format(self.username), 'w', encoding='utf-8') as f:
             json.dump(self.user_info, f, ensure_ascii=False, indent=4)
 
@@ -57,3 +67,4 @@ class Session(object):
 
     def clean_session(self):
         shutil.rmtree(self.user_folder, ignore_errors=True)
+
