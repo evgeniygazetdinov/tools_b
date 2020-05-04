f5f2e4d14acf69b7cd3c0794db164dd699bc8acb
evgesha <zplacesound@gmail.com>
2020-05-04 18:01:57 +0300

    first right implement

--- lib/session_methods.py
+++ lib/session_methods.py
@@ -1,7 +1,18 @@
 import glob
 import os
+from lib.base import send_message
+import json
 
-PATH = os.getcwd()+'/session/'
+
+PATH = os.getcwd()+'/session'
+
+
+def looking_for_changes():
+    last_updaten = check_last_updated_file()
+        #check algoritm there last string changes if change file
+       
+
+
 
 def json_to_dict(path):
     with open(path) as json_file:
@@ -11,22 +22,25 @@ def json_to_dict(path):
 
 def check_last_updated_file():
     #check_last_updated_folder
-    list_of_files = glob.glob(PATH+'/*') # * means all if need specific format then *.csv
+    list_of_files = glob.glob(PATH+'/*') 
     latest_file = max(list_of_files, key=os.path.getctime)
     return latest_file
 
 
-def get_current_user():
+def get_user_from_session():
     file = check_last_updated_file()
     user = os.path.basename(file)
     return str(user)
 
 
-def get_user_action(user):
+def get_action_from_session(user):
     user_path = PATH+'/'+user+'/'+user+'.json'
     user_dict = json_to_dict(user_path)
-    return user_dict
+    return user_dict['cur_chat'],user_dict
     
+def get_current_chat():
+    pass
+
 
 def notify_user(chat):
     pass    
